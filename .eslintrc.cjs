89255cdf9dfe30bff12f315e54ecb85e82ed3db6
Nguyễn Quốc Tiến <72622184+nqtdev@users.noreply.github.com>
2024-10-10 15:05:13 +0700

    Update .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,15 @@
 // Sample Eslint config for React project
 module.exports = {
-  env: { browser: true, es2020: true, node: true },
+  env: {browser: true, es2020: true, node: true},
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended'
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
   ],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
+  parserOptions: {ecmaVersion: 'latest', sourceType: 'module'},
+  settings: {react: {version: '18.2'}},
   plugins: ['react', 'react-hooks', 'react-refresh'],
   rules: {
     'react-refresh/only-export-components': 'warn',
@@ -16,12 +17,10 @@ module.exports = {
     'react-hooks/exhaustive-deps': 'warn',
     'react/prop-types': 0,
     'react/display-name': 0,
-
     'no-console': 1,
     'no-lonely-if': 1,
     'no-unused-vars': 1,
     'space-before-blocks': ['error', 'always'],
-    'object-curly-spacing': [1, 'always'],
     indent: ['warn', 2],
     semi: [1, 'never'],
     quotes: ['error', 'single'],
@@ -30,6 +29,6 @@ module.exports = {
     'keyword-spacing': 1,
     'comma-dangle': 1,
     'comma-spacing': 1,
-    'arrow-spacing': 1
-  }
+    'arrow-spacing': 1,
+  },
 }
